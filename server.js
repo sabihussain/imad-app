44d7837a6eb8268d9a370622c2ff7bee947e640b
sabihussain <30945121+sabihussain@users.noreply.github.com>
2017-09-01 10:28:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-    user: 'coco98',
-    database: 'coco98',
+    user: 'sabihussain17',
+    database: 'sabihussain17',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
