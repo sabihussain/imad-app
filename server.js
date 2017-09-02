fc5c6f0527e2ab0ecce81e2b1988ecfce2256705
sabihussain <30945121+sabihussain@users.noreply.github.com>
2017-09-02 19:11:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,9 @@ app.get('/submit-name',function (req,res) { //URL:/submit-name?name=xxxx
 app.get('/article/:articleName',function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one 
-    pool.query("SELECT * FROM article where title =  " + req.params.articleName, function(err,result) {
+    
+    // SELECT * FROM article where title = 'article-one'
+    pool.query("SELECT * FROM article where title = ' " + req.params.articleName +  "'" , function(err,result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
